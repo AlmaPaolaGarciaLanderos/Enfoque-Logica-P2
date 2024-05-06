e47f516d471098bbffe0ec4666109567466e4a4c
AlmaPaolaGarciaLanderos <162000855+AlmaPaolaGarciaLanderos@users.noreply.github.com>
2024-05-06 12:29:00 -0600

    Practicas P2 IA

---
+++ 010- Lógica de 1er Orden-Reglas de Diagnóstico y Causales.py
@@ -0,0 +1,30 @@
+#Alma Paola Garcia Landeros  
+#21110038  
+#6E1  
+#Inteligencia Artificial  
+#Centro de Enseñanza Tecnica Industrial 
+
+ class SistemaDiagnostico:
+    def __init__(self):
+        self.base_conocimiento = {
+            "Síntoma1": {"Causa1", "Causa2"},
+            "Síntoma2": {"Causa2", "Causa3"},
+            "Síntoma3": {"Causa1", "Causa3"}
+        }
+
+    def diagnosticar(self, sintomas):
+        posibles_causas = set()
+
+        for sintoma in sintomas:
+            if sintoma in self.base_conocimiento:
+                posibles_causas.update(self.base_conocimiento[sintoma])
+
+        return posibles_causas
+
+# Ejemplo de uso
+sistema = SistemaDiagnostico()
+
+sintomas = ["Síntoma1", "Síntoma3"]
+causas_posibles = sistema.diagnosticar(sintomas)
+
+print("Posibles causas:", causas_posibles)
